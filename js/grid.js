4ecaaca5982ae30ac4b4f2666b665dbf7f9705b0
StepanTita <stepan.tytarenko@nure.ua>
2020-02-15 00:02:34 +0100

    chassis

--- js/grid.js
+++ js/grid.js
@@ -1,7 +1,8 @@
 function createGrid() {
     let countRows = config.hoursF - config.hoursS;
 
-    let row = '<div class="dtr"><div class="time-col">08:00</div><div class="time-dtd dtd"></div>'; //<div class="time-col">08:00</div>
+    
+    let row = '<div class="time-dtd dtd"></div>'; //<div class="time-col">08:00</div>
     for (let j = 0; j < config.days + 1; ++j) {
         row += '<div class="dtd"></div>';
     }
@@ -9,21 +10,27 @@ function createGrid() {
 
     let height = 100 / countRows;
 
-    $(".time-table").append(row);
+    let time = '07:00';
+    let timeCol = '<div class="dtr"><div class="time-col"><span>' + time + '</span></div>';
+
+    $(".time-table").append(timeCol + row);
     $(".time-table .dtr").css('height', (4 * height / 5) + '%');
 
     for (let i = 0; i < countRows; ++i) {
+        time = `${config.hoursS + i}:00`.leftJustify(5, '0');
+        timeCol = '<div class="dtr"><div class="time-col"><span>' + time + '</span></div>';
 
-        $(".time-table").append(row);
+        $(".time-table").append(timeCol + row);
         $(".time-table .dtr").last().css('height', height + '%');
     };
 }
 
 function set_days() {
-
+ // TODO
 }
 
 $(document).ready(function() {
     createGrid();
     set_days();
 });
+
---
+++ js/utils.js
@@ -0,0 +1,16 @@
+
+String.prototype.leftJustify = function( length, char ) {
+    var fill = [];
+    while ( fill.length + this.length < length ) {
+      fill[fill.length] = char;
+    }
+    return fill.join('') + this;
+}
+
+String.prototype.rightJustify = function( length, char ) {
+    var fill = [];
+    while ( fill.length + this.length < length ) {
+      fill[fill.length] = char;
+    }
+    return this + fill.join('');
+}
