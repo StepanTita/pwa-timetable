72e3995b5295b3ee19424c4c95f43bc95c84d8b4
StepanTita <stepan.tytarenko@nure.ua>
2020-03-11 23:12:28 +0100

    beta test current time

--- public/js/calendar/timemark.js
+++ public/js/calendar/timemark.js
@@ -22,10 +22,28 @@ function setCurrentTime(today) {
     $('#current-moment').css('height', `${height}%`);
 }
 
+function setDates(today) {
+    let day = today.getDate();
+    let month = today.getMonth();
+    let dayNum = today.getDay();
+
+    let headings = $('.heading');
+    let months = month.toString().leftJustify(2, '0');
+    for (let i = 1; i < dayNum; ++i) {
+        let days = (day - dayNum + i).toString().leftJustify(2, '0');
+        $(headings[i]).find('.date').text(`(${days}.${months})`);
+    }
+    for (let i = dayNum; i < headings.length; ++i) {
+        let days = (day + i - dayNum).toString().leftJustify(2, '0');
+        $(headings[i]).find('.date').text(`(${days}.${months})`);
+    }
+}
+
 function updateTime() {
     let today = new Date();
     setCurrentTime(today);
     setWeekDay(today);
+    setDates(today);
 }
 
 $(document).ready(function () {
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-let V = "3";
+let V = "4";
 const staticCacheName = `site-static-v${V}`;
 const dynamicCacheName = `site-dynamic-v${V}`;
 const assets = [
